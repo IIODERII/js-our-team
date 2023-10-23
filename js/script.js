0b82eec22ff29acd15182335a0a3e11103868c74
IIODERII <oderrisi@gmail.com>
2023-10-23 14:23:34 +0200

    added pictures and array of members

---
+++ js/script.js
@@ -0,0 +1,47 @@
+/*
+
+<div class="col-4">
+        <div class="card text-start">
+          <img class="card-img-top" src="img/angela-caroll-chief-editor.jpg" alt="Title">
+          <div class="card-body">
+            <h4 class="card-title">Nome</h4>
+            <p class="card-text">Ruolo</p>
+          </div>
+        </div>
+</div>
+*/
+
+"use strict";
+
+const teamMembers = [
+  {
+    name: "Wayne Barnett",
+    role: "Founder & CEO",
+    picture: "wayne-barnett-founder-ceo.jpg",
+  },
+  {
+    name: "Angela Caroll",
+    role: "Chief Editor",
+    picture: "angela-caroll-chief-editor.jpg",
+  },
+  {
+    name: "Walter Gordon",
+    role: "Office Manager",
+    picture: "walter-gordon-office-manager.jpg",
+  },
+  {
+    name: "Angela Lopez",
+    role: "Social Media Manager",
+    picture: "angela-lopez-social-media-manager.jpg",
+  },
+  {
+    name: "Scott Estrada",
+    role: "Developer",
+    picture: "scott-estrada-developer.jpg",
+  },
+  {
+    name: "Barbara Ramos",
+    role: "Graphic Designer",
+    picture: "barbara-ramos-graphic-designer.jpg",
+  } 
+];
